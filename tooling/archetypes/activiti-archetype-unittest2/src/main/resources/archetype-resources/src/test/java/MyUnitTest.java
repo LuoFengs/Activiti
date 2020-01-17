aab2d53e838608c4c8f8a7d808e66ab5a1fb8de6
front-luo <front.luo@foxmail.com>
2020-01-17 17:14:10 +0800

    mvn 插件

--- tooling/archetypes/activiti-archetype-unittest2/src/main/resources/archetype-resources/src/test/java/MyUnitTest.java
+++ tooling/archetypes/activiti-archetype-unittest2/src/main/resources/archetype-resources/src/test/java/MyUnitTest.java
@@ -1,11 +1,11 @@
 package ${package};
+
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.activiti.engine.test.ActivitiRule;
 import org.activiti.engine.test.Deployment;
 import org.junit.Rule;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 
